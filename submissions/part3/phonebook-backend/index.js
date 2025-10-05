83de275eb0bed954cfdef07822d971c5b04986e0
Luca Carrozzo <carrozzo.lc@gmail.com>
2025-10-05 21:43:16 +0200

    minor fix

--- submissions/part3/phonebook-backend/index.js
+++ submissions/part3/phonebook-backend/index.js
@@ -35,6 +35,7 @@ app.use(express.json());
 app.use(
   morgan(':method :url :status :res[content-length] - :response-time ms')
 );
+app.use(express.static('dist'));
 
 app.get('/', function (req, res) {
   res.send('hello, world!');

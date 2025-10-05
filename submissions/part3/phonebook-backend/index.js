d58135a37bc3a763bff6c62cef3fc31cd4cc4813
Luca Carrozzo <carrozzo.lc@gmail.com>
2025-10-05 21:35:12 +0200

    exercises part 3b

--- submissions/part3/phonebook-backend/index.js
+++ submissions/part3/phonebook-backend/index.js
@@ -108,7 +108,7 @@ app.post('/api/persons', morgan(':body'), (request, response) => {
   response.status(201).json(person);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
